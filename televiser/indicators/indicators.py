def770d069283652b51ad69c1c742d72fef9b4a5
mrsnurk32 <nickbelik@mail.ru>
2021-02-03 09:10:30 +0300

    Added: 	1. EMA indicatord 	2. MACD indicator 	3. Stockhastic indicator

--- televiser/indicators/indicators.py
+++ televiser/indicators/indicators.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 __all__ = ['get_indicator']
 
@@ -6,6 +7,7 @@ __all__ = ['get_indicator']
 class Indicators:
 
     def moving_average(self, frame, period=24):
+
         print('MA')
         frame = frame['frame']
         frame['MA24'] = frame.Close.rolling(period).mean()
@@ -14,7 +16,20 @@ class Indicators:
 
 
     def ema(self, frame, period=12):
-        pass
+
+        frame = frame['frame']
+        frame['EMA12'] = frame.Close.ewm(span=12).mean()
+        # frame['EMA26'] = frame.Close.ewm(span=26).mean()
+        frame['Criteria'] = frame['EMA12'] > frame.Close
+        return frame
+
+
+    # def bollinger_bands(self, frame):
+    #     frame = frame['frame']
+    #     frame['MA20'] = frame.Close.rolling(20).mean()
+    #     frame['STD20'] = frame.Close.rolling(20).std()
+    #     frame['lower_band'] = frame['MA20'] - frame['STD20'] * 2
+    #     frame['upper_band'] = frame['MA20'] + frame['STD20'] * 2
 
 
     def rsi(self, frame):
@@ -26,7 +41,15 @@ class Indicators:
 
 
     def stockhastic(self, frame):
-        pass
+
+        frame = frame['frame']
+        frame['Min'] = frame.Low.rolling(14).min()
+        frame['close-min'] = frame.Close - frame.Min
+        frame['H-L'] = frame.High.rolling(14).max() - frame.Low.rolling(14).min()
+        frame['K'] = frame['close-min'] / frame['H-L'] * 100
+        frame['SlowK'] = (frame['close-min'].rolling(3).sum() / frame['H-L'].rolling(3).sum()) * 100
+        frame['Criteria'] = np.where((frame.SlowK > 25) & (frame.SlowK < 85), True, False )
+        return frame
 
 
     def standard_deviation(self, frame, period = 24):
@@ -34,7 +57,16 @@ class Indicators:
 
 
     def macd(self, frame):
-        pass
+
+        frame = frame['frame']
+        frame['EMA12'] = frame.Close.ewm(span=12).mean()
+        frame['EMA26'] = frame.Close.ewm(span=26).mean()
+        frame['Difference'] = frame.EMA12 - frame.EMA26
+        frame['Signal'] = frame.Difference.ewm(span=9).mean() #9 period ema
+        frame['Histogram'] = frame.Difference - frame.Signal
+        frame['Criteria'] = frame.Histogram > 0
+
+        return frame
 
 
     def slope(self, frame, period, angle):
