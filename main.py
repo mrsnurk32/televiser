beb8d4506587d4be2f5ea9a092f33a026e6318db
mrsnurk32 <nickbelik@mail.ru>
2021-02-05 01:23:59 +0300

    (try) code was moved to (else) part

--- main.py
+++ main.py
@@ -24,30 +24,29 @@ def step1(message):
     menu1.add(telebot.types.InlineKeyboardButton(text = 'EMA', callback_data ='ema'))
     menu1.add(telebot.types.InlineKeyboardButton(text = 'Stockhastic', callback_data ='stockhastic'))    
    
-
-
     start = dt(2018,1,1)
     end = dt.today().date()
 
     try:
         frame = web.DataReader(message.text, 'yahoo',start,end)
-        stock_data = f'Stock name: {message.text}\nopen:{frame.Open.iloc[-1]}\nhigh: {frame.High.iloc[-1]}\nlow: {frame.Low.iloc[-1]}\nclose:{frame.Close.iloc[-1]}'
-
-        global chat_list
-        chats[message.chat.id] = SimpleBackTest(frame, stock_data)
-        msg = bot.send_message(message.chat.id, text ='Выберите тех. индикатор', reply_markup = menu1)
-
 
     except Exception as err:
         print(err)
         bot.send_message(message.chat.id,'No stock data')
 
+    else:
+        stock_data = f'Stock name: {message.text.upper()}\nopen:{frame.Open.iloc[-1]}\nhigh: {frame.High.iloc[-1]}\nlow: {frame.Low.iloc[-1]}\nclose:{frame.Close.iloc[-1]}'
+        global chat_list
+        chats[message.chat.id] = SimpleBackTest(frame, stock_data, message.text)
+        msg = bot.send_message(message.chat.id, text ='Выберите тех. индикатор', reply_markup = menu1)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def step2(call):
-    print(call.data)
+
     data = chats[call.message.chat.id]
     bot.send_photo(call.message.chat.id, data(indicator=call.data))
+    bot.send_message(call.message.chat.id,text = data.ev_message)
 
 
 bot.polling(none_stop=True)
